2e2568b0e0d00834f636ad5bc211801bb0ae02ea
VagRant2333 <helloforrest23@gmail.com>
2026-06-22 21:30:06 +0800

    添加：人工智能H（郑骁庆）课程笔记

--- pages/courses/_meta.ts
+++ pages/courses/_meta.ts
@@ -26,6 +26,7 @@ export default {
   'shujukuyinlun': '🗄️ 数据库引论',
   'moshudianshiyan': '🔧 模数电实验',
   'rengongzhinengjichu': '🤖 人工智能基础',
+  'rengongzhinengH': '🤖 人工智能 H（郑骁庆）',
   'daishujiegouyushuliluoji': '📚 代数结构与数理逻辑',
   'ziranyuyanchuli': '🤖 自然语言处理',
 
